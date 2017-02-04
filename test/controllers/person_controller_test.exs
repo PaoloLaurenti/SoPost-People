3159defab94f74c14ff040d5bcb6ddb33315f82b
Paolo Laurenti <laurentipaolo@gmail.com>
2017-02-04 11:56:37 +0100

    Person controller test refactoring

--- test/controllers/person_controller_test.exs
+++ test/controllers/person_controller_test.exs
@@ -29,23 +29,33 @@ defmodule SopostPeople.PersonControllerTest do
 
     response = get conn, person_path(conn, :index, %{location: "location1"})
 
-    people()
-      |> Enum.filter(fn(person) -> person.location == "location1" end)
+    people_in_location1()
       |> Enum.each(fn(person) ->
         assert html_response(response, 200) =~ person.name
         assert html_response(response, 200) =~ person.role
         assert html_response(response, 200) =~ person.location
         assert html_response(response, 200) =~ person.photo
       end)
-      refute html_response(response, 200) =~ "name2"
-      refute html_response(response, 200) =~ "role2"
-      refute html_response(response, 200) =~ "location2"
-      refute html_response(response, 200) =~ "http://photo2.png"
+    people_in_location2()
+      |> Enum.each(fn(person) ->
+        refute html_response(response, 200) =~ person.name
+        refute html_response(response, 200) =~ person.role
+        refute html_response(response, 200) =~ person.location
+        refute html_response(response, 200) =~ person.photo
+      end)
   end
 
   defp people() do
+    Enum.concat(people_in_location1(), people_in_location2())
+  end
+
+  defp people_in_location1 do
     [ %{name: "name1", role: "role1", location: "location1", photo: "http://photo1.png" },
-      %{name: "name2", role: "role2", location: "location2", photo: "http://photo2.png" },
       %{name: "name3", role: "role3", location: "location1", photo: "http://photo3.png" }]
+  end
+
+  defp people_in_location2 do
+    [ %{name: "name2", role: "role2", location: "location2", photo: "http://photo2.png" },
+      %{name: "name4", role: "role4", location: "location2", photo: "http://photo4.png" } ]
   end
 end
